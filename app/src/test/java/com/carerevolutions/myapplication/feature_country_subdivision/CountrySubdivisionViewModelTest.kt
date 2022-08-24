16ef5f882d6fce79d80f2b8313715cd7b3fcc92e
jonathanregalado <jonathan.regalado@carerev.com>
2022-08-24 11:01:20 -0700

    Added unit test stub class

---
+++ app/src/test/java/com/carerevolutions/myapplication/feature_country_subdivision/CountrySubdivisionViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.carerevolutions.myapplication.feature_country_subdivision
+
+import org.junit.Test
+
+internal class CountrySubdivisionViewModelTest {
+
+    @Test
+    fun loadCountrySubdivisionData() {
+        /** Fix Me.. */
+    }
+}
